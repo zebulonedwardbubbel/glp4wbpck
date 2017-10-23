e6fed8e1f8cacdf8005d55dad8234f0584889b03
Szabolcs Kertész <sz.kertesz@mito.hu>
2017-10-23 19:46:50 +0200

    Add ESlint

--- tasks/webpack.js
+++ tasks/webpack.js
@@ -14,6 +14,12 @@ let config = {
     module: {
         rules: [
             {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
